ca793f3c82fb927781956d200093c5874f97a936
jahupe <jahupe@gmail.com>
2022-01-09 20:56:28 -0500

    Both_Modifications
    -

---
+++ BikeStore.Core/Enumerations/UserType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeStore.Core.Enumerations
+{
+    public enum UserType
+    {
+        Administrador,
+        Reader,
+    }
+}
